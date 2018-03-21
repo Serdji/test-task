2773a30482c7e9b7fb58a7d772d0d6349d3c536f
sergey <c.c@bk.ru>
2018-03-21 21:16:33 +0300

    Первоначальная разметка

---
+++ src/app/form-book/form-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-book',
+  templateUrl: './form-book.component.html',
+  styleUrls: ['./form-book.component.styl']
+})
+export class FormBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main/item-book/item-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-book',
+  templateUrl: './item-book.component.html',
+  styleUrls: ['./item-book.component.styl']
+})
+export class ItemBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
