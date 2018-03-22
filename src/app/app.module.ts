6ab8a5e6b2ee40d0c08e8f2c954159f44f5a0afe
sumatokhin <sumatokhin@adv>
2018-03-22 09:22:02 +0300

    Реактивана форма с первичной валидацией

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,7 +19,8 @@ import { ItemBookComponent } from './main/item-book/item-book.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form-book/form-book.component.ts
+++ src/app/form-book/form-book.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-book',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormBookComponent implements OnInit {
 
-  constructor() { }
+  formBook: FormGroup;
+
+  constructor(
+    private fd: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.formBook = this.fd.group({
+      title: ['', [ Validators.required, Validators.maxLength(30) ]],
+      name: ['', [Validators.required, Validators.maxLength(20) ]],
+      surname: ['', [Validators.required, Validators.maxLength(20) ]],
+      pages: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(10000), Validators.pattern(/[0-9]/)] ],
+      edition: ['', [Validators.maxLength(30) ]],
+      publication: ['', [ Validators.pattern(/[0-9]/)] ],
+    });
+  }
+
+  sendForm(): void {
+    for (const inner of Object.keys(this.formBook.controls)) {
+      this.formBook.get(inner).markAsTouched();
+    }
+  }
+
+  goBack(): void{
+    this.router.navigate(['/']);
   }
 
 }
